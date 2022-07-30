a9b43c2ca2b572377bc4500150e8faeca1bf545f
Jason Zheng <jasonz4200@gmail.com>
2022-07-30 03:41:20 -0500

    Add tmpdata to build path

--- src/window.py
+++ src/window.py
@@ -16,6 +16,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from gi.repository import Gtk, Adw
+from .tmpdata import course_data
 
 
 @Gtk.Template(resource_path='/com/github/jasozh/Canari/window.ui')
@@ -25,86 +26,12 @@ class CanariWindow(Gtk.ApplicationWindow):
     welcome_screen = Gtk.Template.Child()
     main_screen = Gtk.Template.Child()
     course_list_box = Gtk.Template.Child()
-    course_data = [
-        {
-            'name': 'ENGRG 1028 (CS 2800 AEW) SEM 101',
-            'course_id': '19233',
-            'next_id': '19234',
-            'url': 'https://classes.cornell.edu/browse/roster/SP22/class/ENGRG/1028',
-            'status': 'open',
-            'last_update': 'time obj here'
-        },
-        {
-            'name': 'ENGRG 1028 (CS 2800 AEW) SEM 102',
-            'course_id': '19234',
-            'next_id': 'end',
-            'url': 'https://classes.cornell.edu/browse/roster/SP22/class/ENGRG/1028',
-            'status': 'closed',
-            'last_update': 'time obj here'
-        },
-        {
-            'name': 'FREN 1220 DIS 204',
-            'course_id': '4064',
-            'next_id': '4065',
-            'url': 'https://classes.cornell.edu/browse/roster/SP22/class/FREN/1220',
-            'status': 'closed',
-            'last_update': 'time obj here'
-        },
-        {
-            'name': 'CS 2800 DIS 205',
-            'course_id': '10595',
-            'next_id': '10596',
-            'url': 'https://classes.cornell.edu/browse/roster/SP22/class/CS/2800',
-            'status': 'closed',
-            'last_update': 'time obj here'
-        },
-        {
-            'name': 'CS 2800 DIS 212',
-            'course_id': '10603',
-            'next_id': '10604',
-            'url': 'https://classes.cornell.edu/browse/roster/SP22/class/CS/2800',
-            'status': 'closed',
-            'last_update': 'time obj here'
-        },
-        {
-            'name': 'CS 2800 DIS 211 (test of open class)',
-            'course_id': '10602',
-            'next_id': '10603',
-            'url': 'https://classes.cornell.edu/browse/roster/SP22/class/CS/2800',
-            'status': 'closed',
-            'last_update': 'time obj here'
-        },
-        {
-            'name': 'AAS 2620 (test of open class)',
-            'course_id': '17500',
-            'next_id': 'end',
-            'url': 'https://classes.cornell.edu/browse/roster/SP22/class/AAS/2620',
-            'status': 'closed',
-            'last_update': 'time obj here'
-        },
-        {
-            'name': 'CS 2110 DIS 214',
-            'course_id': '10269',
-            'next_id': '10271',
-            'url': 'https://classes.cornell.edu/browse/roster/SP22/class/CS/2110',
-            'status': 'closed',
-            'last_update': 'time obj here'
-        },
-        # {
-        #     'name': '',
-        #     'course_id': '',
-        #     'next_id': '',
-        #     'url': '',
-        #     'status': 'open',
-        #     'last_update': 'time obj here'
-        # },
-    ]
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self.show_content(self.course_data)
-        self.show_tracked_courses(self.course_data)
+        self.show_content(course_data)
+        self.show_tracked_courses(course_data)
 
     def show_content(self, course_list: list) -> None:
         """
