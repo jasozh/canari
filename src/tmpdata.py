264709acf59420bf5dde7be08e375dd332d702d1
Jason Zheng <jasonz4200@gmail.com>
2022-07-30 03:31:14 -0500

    Add dummy data and adwactionrow functionality

---
+++ src/tmpdata.py
@@ -0,0 +1,74 @@
+course_data = [
+    {
+        'name': 'ENGRG 1028 (CS 2800 AEW) SEM 101',
+        'course_id': '19233',
+        'next_id': '19234',
+        'url': 'https://classes.cornell.edu/browse/roster/SP22/class/ENGRG/1028',
+        'status': 'closed',
+        'last_update': 'time obj here'
+    },
+    {
+        'name': 'ENGRG 1028 (CS 2800 AEW) SEM 102',
+        'course_id': '19234',
+        'next_id': 'end',
+        'url': 'https://classes.cornell.edu/browse/roster/SP22/class/ENGRG/1028',
+        'status': 'closed',
+        'last_update': 'time obj here'
+    },
+    {
+        'name': 'FREN 1220 DIS 204',
+        'course_id': '4064',
+        'next_id': '4065',
+        'url': 'https://classes.cornell.edu/browse/roster/SP22/class/FREN/1220',
+        'status': 'closed',
+        'last_update': 'time obj here'
+    },
+    {
+        'name': 'CS 2800 DIS 205',
+        'course_id': '10595',
+        'next_id': '10596',
+        'url': 'https://classes.cornell.edu/browse/roster/SP22/class/CS/2800',
+        'status': 'closed',
+        'last_update': 'time obj here'
+    },
+    {
+        'name': 'CS 2800 DIS 212',
+        'course_id': '10603',
+        'next_id': '10604',
+        'url': 'https://classes.cornell.edu/browse/roster/SP22/class/CS/2800',
+        'status': 'closed',
+        'last_update': 'time obj here'
+    },
+    {
+        'name': 'CS 2800 DIS 211 (test of open class)',
+        'course_id': '10602',
+        'next_id': '10603',
+        'url': 'https://classes.cornell.edu/browse/roster/SP22/class/CS/2800',
+        'status': 'closed',
+        'last_update': 'time obj here'
+    },
+    {
+        'name': 'AAS 2620 (test of open class)',
+        'course_id': '17500',
+        'next_id': 'end',
+        'url': 'https://classes.cornell.edu/browse/roster/SP22/class/AAS/2620',
+        'status': 'closed',
+        'last_update': 'time obj here'
+    },
+    {
+        'name': 'CS 2110 DIS 214',
+        'course_id': '10269',
+        'next_id': '10271',
+        'url': 'https://classes.cornell.edu/browse/roster/SP22/class/CS/2110',
+        'status': 'closed',
+        'last_update': 'time obj here'
+    },
+    # {
+    #     'name': '',
+    #     'course_id': '',
+    #     'next_id': '',
+    #     'url': '',
+    #     'status': 'open',
+    #     'last_update': 'time obj here'
+    # },
+]
--- src/window.py
+++ src/window.py
@@ -24,14 +24,137 @@ class CanariWindow(Gtk.ApplicationWindow):
 
     welcome_screen = Gtk.Template.Child()
     main_screen = Gtk.Template.Child()
-    actionrow = Gtk.Template.Child()
+    course_list_box = Gtk.Template.Child()
+    course_data = [
+        {
+            'name': 'ENGRG 1028 (CS 2800 AEW) SEM 101',
+            'course_id': '19233',
+            'next_id': '19234',
+            'url': 'https://classes.cornell.edu/browse/roster/SP22/class/ENGRG/1028',
+            'status': 'open',
+            'last_update': 'time obj here'
+        },
+        {
+            'name': 'ENGRG 1028 (CS 2800 AEW) SEM 102',
+            'course_id': '19234',
+            'next_id': 'end',
+            'url': 'https://classes.cornell.edu/browse/roster/SP22/class/ENGRG/1028',
+            'status': 'closed',
+            'last_update': 'time obj here'
+        },
+        {
+            'name': 'FREN 1220 DIS 204',
+            'course_id': '4064',
+            'next_id': '4065',
+            'url': 'https://classes.cornell.edu/browse/roster/SP22/class/FREN/1220',
+            'status': 'closed',
+            'last_update': 'time obj here'
+        },
+        {
+            'name': 'CS 2800 DIS 205',
+            'course_id': '10595',
+            'next_id': '10596',
+            'url': 'https://classes.cornell.edu/browse/roster/SP22/class/CS/2800',
+            'status': 'closed',
+            'last_update': 'time obj here'
+        },
+        {
+            'name': 'CS 2800 DIS 212',
+            'course_id': '10603',
+            'next_id': '10604',
+            'url': 'https://classes.cornell.edu/browse/roster/SP22/class/CS/2800',
+            'status': 'closed',
+            'last_update': 'time obj here'
+        },
+        {
+            'name': 'CS 2800 DIS 211 (test of open class)',
+            'course_id': '10602',
+            'next_id': '10603',
+            'url': 'https://classes.cornell.edu/browse/roster/SP22/class/CS/2800',
+            'status': 'closed',
+            'last_update': 'time obj here'
+        },
+        {
+            'name': 'AAS 2620 (test of open class)',
+            'course_id': '17500',
+            'next_id': 'end',
+            'url': 'https://classes.cornell.edu/browse/roster/SP22/class/AAS/2620',
+            'status': 'closed',
+            'last_update': 'time obj here'
+        },
+        {
+            'name': 'CS 2110 DIS 214',
+            'course_id': '10269',
+            'next_id': '10271',
+            'url': 'https://classes.cornell.edu/browse/roster/SP22/class/CS/2110',
+            'status': 'closed',
+            'last_update': 'time obj here'
+        },
+        # {
+        #     'name': '',
+        #     'course_id': '',
+        #     'next_id': '',
+        #     'url': '',
+        #     'status': 'open',
+        #     'last_update': 'time obj here'
+        # },
+    ]
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.welcome_screen.hide()
-        self.main_screen.show()
-        # self.actionrow.add_suffix(Gtk.Label(label="Test"))
-        # self.actionrow.add_prefix(Gtk.Button(label="Test", icon_name="open-menu-symbolic"))
+
+        self.show_content(self.course_data)
+        self.show_tracked_courses(self.course_data)
+
+    def show_content(self, course_list: list) -> None:
+        """
+        If course_list is empty, show the Welcome Screen. Otherwise, show the
+        Main Screen components
+        """
+        if len(course_list) > 0:
+            self.welcome_screen.hide()
+            self.main_screen.show()
+        else:
+            self.welcome_screen.show()
+            self.main_screen.hide()
+
+    def show_tracked_courses(self, course_list: list) -> None:
+        """
+        Adds AdwActionRow children to course_list_box to render tracked courses.
+        Each element in course_list is a dict with the following keys:
+            name, course_id, url, status, last_update
+
+        The properties of the AdwActionRow are instantiated in the following order:
+            icon-name, title, subtitle, child
+        """
+        for item in course_list:
+            course_row = Adw.ActionRow()
+
+            # icon-name
+            if (item['status'] == 'closed'):
+                course_row.set_icon_name('dialog-error-symbolic')
+            elif (item['status'] == 'open'):
+                course_row.set_icon_name('software-update-urgent-symbolic')
+
+            # title
+            course_row.set_title(item['name'])
+            if (item['status'] == 'open'):
+                course_row.set_css_classes(['heading'])
+
+            # subtitle
+            course_row.set_subtitle(f'Last updated at {item["last_update"]}.')
+
+            # child
+            status_label = Gtk.Label()
+            if (item['status'] == 'closed'):
+                status_label.set_label(item['status'].title())
+            elif (item['status'] == 'open'):
+                status_label.set_label(item['status'].upper())
+
+            course_row.add_suffix(status_label)
+            # course_row.add_prefix(Gtk.Button(label="Test", icon_name="open-menu-symbolic"))
+
+            self.course_list_box.append(course_row)
 
 
 class AboutDialog(Gtk.AboutDialog):
